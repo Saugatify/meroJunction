df05b455f35fcbd4ddc35b6fb4166e54cd12366d
Saugat <saugatg189@gmail.com>
2024-09-13 08:13:27 +0545

    junctions added

--- junction.js
+++ junction.js
@@ -1,12 +1,8 @@
-// junctions.js
 const junctions = [
-  { name: "Thamel", latitude: 27.7154, longitude: 85.3123, iconUrl: '/images/hotel.png' },
-  { name: "Bouddhanath Stupa", latitude: 27.7215, longitude: 85.3620, iconUrl: '/images/temple.png' },
-  { name: "Patan Durbar Square", latitude: 27.6730, longitude: 85.3250, iconUrl: 'path/to/patan-icon.png' },
-  { name: "Garden of Dreams", latitude: 27.7138, longitude: 85.3161, iconUrl: 'path/to/garden-icon.png' },
-  { name: "Durbar Marg", latitude: 27.7136, longitude: 85.3173, iconUrl: 'path/to/durbar-icon.png' },
   { name: "Chiya Ghar", latitude: 27.7100, longitude: 85.3130, iconUrl: '/images/chiya_ghar.png' },
-  { name: "Chiya Laya", latitude: 27.7120, longitude: 85.3140, iconUrl: '/images/chiya_laya.png' },
+  { name: "Chiya Laya", latitude: 27.7120, longitude: 85.3140, iconUrl: '/images/chiya_ghar.png' },
+  { name: "Jiggs Burger", latitude: 27.6947, longitude: 85.3401, iconUrl: '/images/jiggs_burger.png' },
+  { name: "Chiya Dani", latitude: 27.6847, longitude: 85.3352, iconUrl: '/images/chiya_dani.png' },
 ];
 
 export default junctions;
